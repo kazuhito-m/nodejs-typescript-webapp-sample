9a5bfea2b937553aec913d9709ba0d7ad18c5bf1
kazuhito-m <kazuhito.sumpic@gmail.com>
2019-01-29 20:20:02 +0900

    refactor: 標準的なTypeScript構造へと、ディレクトリ構造を移動。

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const nodeExternals = require('webpack-node-externals')
 
 module.exports = {
   mode: 'production',
-  entry: __dirname + "/src/main/ts/index.ts", //ビルドするファイル
+  entry: __dirname + "/src/index.ts", //ビルドするファイル
   output: {
     path: __dirname + '/dist', //ビルドしたファイルを吐き出す場所
     filename: 'bundle.js' //ビルドした後のファイル名
