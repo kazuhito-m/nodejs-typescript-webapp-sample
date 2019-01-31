b4e32ec8f80643a88f53a3da950c777963e272b8
kazuhito-m <kazuhito.sumpic@gmail.com>
2019-02-01 03:17:22 +0900

    refactor: WebApplicationクラスのメソッドを、expresの設定と実行に分ける。

--- src/WebApplication.ts
+++ src/WebApplication.ts
@@ -1,6 +1,8 @@
 import * as bodyParser from 'body-parser';
+import { Application } from 'express';
 import { InversifyExpressServer } from 'inversify-express-utils';
 import DIContainerBuilder from './DIContainerBuilder';
+import Settings from './domain/config/Settings';
 import Parameters from './infrastracture/datasource/config/Parameters';
 
 export default class WebApplication {
@@ -9,19 +11,25 @@ export default class WebApplication {
     parameters.analyzeArgs();
     const settings = parameters.loadSettings();
 
+    const application = this.buildExpressApplication(settings);
+
+    application.listen(settings.port, () => {
+      console.log('Node.js & Express is listening to Port:' + settings.port);
+    });
+  }
+
+  public buildExpressApplication(settings: Settings): Application {
     const builder = new DIContainerBuilder(settings);
     const container = builder.build();
 
     const rootConfig = { rootPath: '/api/v1' };
     const server = new InversifyExpressServer(container, null, rootConfig);
-    server.setConfig((app) => {
+
+    server.setConfig(app => {
       app.use(bodyParser.urlencoded({ extended: true }));
       app.use(bodyParser.json());
     });
 
-    const application = server.build();
-    application.listen(settings.port, () => {
-      console.log('Node.js & Express is listening to Port:' + settings.port);
-    });
+    return server.build();
   }
 }
