567f057928ffdd2ba03aab5deabea1e01a75dbae
kazuhito-m <kazuhito.sumpic@gmail.com>
2019-02-10 03:19:04 +0900

    実装。migrateionsファイルのパスを組み立てるように。

--- env/local/db/migration/migrate.js
+++ env/local/db/migration/migrate.js
@@ -1,31 +1,30 @@
 const {
     createDb,
     migrate
-} = require("postgres-migrations")
+} = require("postgres-migrations");
+const path = require('path');
 
-console.log('__dirnameだけど…:' + __dirname);
+const migrateionsFilePath = path.join(__dirname, 'migrations');
 
-/*
-createDb("database-name", {
-        defaultDatabase: "postgres", // optional, default: "postgres"
-        user: "postgres",
-        password: "password",
-        host: "localhost",
-        port: 5432,
-    })
-    .then(() => {
-        return migrate({
-            database: "database-name",
-            user: "postgres",
-            password: "password",
-            host: "localhost",
-            port: 5432,
-        }, "path/to/migration/files")
-    })
-    .then(() => {
-        console.log('ここに来るときは？');
-    })
-    .catch((err) => {
-        console.log(err)
-    })
-*/
+// createDb("database-name", {
+//         defaultDatabase: "postgres", // optional, default: "postgres"
+//         user: "postgres",
+//         password: "password",
+//         host: "localhost",
+//         port: 5432,
+//     })
+//     .then(() => {
+//         return migrate({
+//             database: "database-name",
+//             user: "postgres",
+//             password: "password",
+//             host: "localhost",
+//             port: 5432,
+//         }, migrateionsFilePath);
+//     })
+//     .then(() => {
+//         console.log('ここに来るときは？');
+//     })
+//     .catch((err) => {
+//         console.log(err)
+//     });
