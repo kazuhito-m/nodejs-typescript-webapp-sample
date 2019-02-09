a2fe33a3f4e77977d747117fc573121588a2770a
kazuhito-m <kazuhito.sumpic@gmail.com>
2019-02-10 04:04:50 +0900

    実装。マイグレーションをしているところを「設定ファイルから接続設定を取得し、マイグレーションをかける」ように。

--- env/local/db/migration/migrate.js
+++ env/local/db/migration/migrate.js
@@ -4,27 +4,15 @@ const {
 } = require("postgres-migrations");
 const path = require('path');
 
+// DB接続用設定を設定ファイルより抽出。
+const configFilePath = path.join(__dirname, '..', '..', '..', '..', 'config', 'settings.json');
+const pgDbSettings = require(configFilePath).pg;
+pgDbSettings.defaultDatabase = pgDbSettings.database;
+
+// マイグレーション用DDLの取得先。
 const migrateionsFilePath = path.join(__dirname, 'migrations');
 
-// createDb("database-name", {
-//         defaultDatabase: "postgres", // optional, default: "postgres"
-//         user: "postgres",
-//         password: "password",
-//         host: "localhost",
-//         port: 5432,
-//     })
-//     .then(() => {
-//         return migrate({
-//             database: "database-name",
-//             user: "postgres",
-//             password: "password",
-//             host: "localhost",
-//             port: 5432,
-//         }, migrateionsFilePath);
-//     })
-//     .then(() => {
-//         console.log('ここに来るときは？');
-//     })
-//     .catch((err) => {
-//         console.log(err)
-//     });
+createDb(pgDbSettings.database, pgDbSettings)
+    .then(() => migrate(pgDbSettings, migrateionsFilePath))
+    .then(() => console.log('DB Migrteion success!'))
+    .catch((err) => console.log(err));
