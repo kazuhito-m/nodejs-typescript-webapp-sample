f69f260c9ebe35753f7633b34c8f95bbad0ac163
kazuhito-m <kazuhito.sumpic@gmail.com>
2019-03-24 17:02:15 +0900

    bugfix: 順番が非同期にならないようasync/awaitを追加する。

--- nestjs-webapi-sample/src/presentation/controller/logging.interceptor.ts
+++ nestjs-webapi-sample/src/presentation/controller/logging.interceptor.ts
@@ -16,31 +16,33 @@ export class LoggingInterceptor implements NestInterceptor<any, any> {
     private readonly operationHistoryService: OperationHistoryService,
   ) {}
 
-  public intercept(
+  public async intercept(
     context: ExecutionContext,
     next: CallHandler<any>,
-  ): Observable<any> {
+  ): Promise<Observable<any>> {
     const now = Date.now();
 
-    this.registerOperationHistory(context);
+    await this.registerOperationHistory(context);
 
-    return next.handle().pipe(
-      tap(() => {
+    return await next.handle().pipe(
+      tap(async () => {
         const logText = `リクエスト返却完了。経過時間 : ${Date.now() - now}ms`;
         this.logger.debug(logText);
       }),
     );
   }
 
-  private registerOperationHistory(context: ExecutionContext) {
+  private async registerOperationHistory(
+    context: ExecutionContext,
+  ): Promise<void> {
     const hitTest = context
       .getArgs()
       .filter(arg => arg.constructor.name === 'IncomingMessage');
     const url = hitTest.length > 0 ? hitTest[0].url : '';
     const name = context.getClass().name + '.' + context.getHandler().name;
-    this.operationLogger.info(url + ',' + name);
+    await this.operationLogger.info(url + ',' + name);
 
     const operationHistory = OperationHistory.of(url, name);
-    this.operationHistoryService.register(operationHistory);
+    await this.operationHistoryService.register(operationHistory);
   }
 }
