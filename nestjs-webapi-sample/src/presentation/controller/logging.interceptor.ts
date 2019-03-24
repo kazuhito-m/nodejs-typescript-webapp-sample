8a1e27497ffce37efdad26e4bf5ae7addcbbe485
kazuhito-m <kazuhito.sumpic@gmail.com>
2019-03-24 11:36:58 +0900

    実装。OperationLogという形のログを出力するように。#7

--- nestjs-webapi-sample/src/presentation/controller/logging.interceptor.ts
+++ nestjs-webapi-sample/src/presentation/controller/logging.interceptor.ts
@@ -3,9 +3,13 @@ import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
 import { OperationHistoryService } from 'src/application/serivce/operationhistory/operation.history.service';
 import OperationHistory from 'src/domain/model/operationhistory/operation.history';
+import * as Log4js from '../../../node_modules/log4js/lib/log4js';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
+  private readonly logger = Log4js.getLogger();
+  private readonly operationLogger = Log4js.getLogger('operationLogger');
+
   constructor(
     private readonly operationHistoryService: OperationHistoryService,
   ) {}
@@ -16,11 +20,14 @@ export class LoggingInterceptor implements NestInterceptor {
   ): Observable<any> {
     this.registerOperationHistory(context);
 
+    if (!this.logger.isDebugEnabled()) return next.pipe();
+
     const now = Date.now();
     return next.pipe(
-      tap(() =>
-        console.log(`リクエスト返却完了。経過時間 : ${Date.now() - now}ms`),
-      ),
+      tap(() => {
+        const logText = `リクエスト返却完了。経過時間 : ${Date.now() - now}ms`;
+        this.logger.debug(logText);
+      }),
     );
   }
 
@@ -30,6 +37,7 @@ export class LoggingInterceptor implements NestInterceptor {
       .filter(arg => arg.constructor.name === 'IncomingMessage');
     const url = hitTest.length > 0 ? hitTest[0].url : '';
     const name = context.getClass().name + '.' + context.getHandler().name;
+    this.operationLogger.info(url + ',' + name);
 
     const operationHistory = OperationHistory.of(url, name);
     this.operationHistoryService.register(operationHistory);
