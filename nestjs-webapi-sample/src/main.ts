1508595522f46f17fa13f465f443fffe332c0fbd
kazuhito-m <kazuhito.sumpic@gmail.com>
2019-03-24 10:30:38 +0900

    実装。Expressのログを出力するように。#7

--- nestjs-webapi-sample/src/main.ts
+++ nestjs-webapi-sample/src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import Log4jsAdapterService from './Log4jsAdapterService';
+import * as Log4js from '../node_modules/log4js/lib/log4js';
+import { INestApplication } from '@nestjs/common';
+
+function setLog4jsLoggerToExpress(app: INestApplication) {
+  const logger = Log4js.getLogger('access');
+  app.use(Log4js.connectLogger(logger, {}));
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: new Log4jsAdapterService(),
   });
+  setLog4jsLoggerToExpress(app);
   await app.listen(3000);
 }
+
 bootstrap();
